8ccd7746fd0bb066248003d944c6a6512170dd8c
Untitled Master <128633214+Untitled-Master@users.noreply.github.com>
2024-03-16 23:32:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,50 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+@app.route('/anime_episodes2', methods=['GET'])
+def anime_episodes():
+    anime = request.args.get('anime')
+    url = f'https://xsaniime.com/anime/{anime}/'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the div element with id "episodes"
+        episodes_div = soup.find('div', id='episodes')
+        anime_data = []
+
+        if episodes_div:
+            # Find all the li elements within the episodes_div
+            episodes_list = episodes_div.find_all('li')
+
+            for episode_li in episodes_list:
+                # Extract the URL and episode name from the a element
+                episode_url = episode_li.a['href']
+                episode_name = episode_li.a['title']
+
+                # Scrape the episode URL and find all data-embed attributes
+                episode_response = requests.get(episode_url)
+                if episode_response.status_code == 200:
+                    episode_soup = BeautifulSoup(episode_response.text, 'html.parser')
+                    data_embed_links = episode_soup.find_all(attrs={"data-embed": True})
+                    data_embed = [link['data-embed'] for link in data_embed_links]
+                else:
+                    data_embed = []
+
+                # Append the episode data to anime_data
+                anime_data.append({
+                    'title': episode_name,
+                    'urls': data_embed,
+                })
+
+            return jsonify(anime_data)
+        else:
+            return jsonify({'message': 'No episodes found.'}), 404
+    else:
+        return jsonify({'message': 'Failed to fetch the webpage.'}), 500
+
 @app.route('/anime_data', methods=['GET'])
 def get_anime_data():
     url = 'https://x.xsanime.com/episodes/'
