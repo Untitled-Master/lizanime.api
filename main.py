6a0e0c027743b96ef5525d88d118cedb70e323ae
Untitled Master <128633214+Untitled-Master@users.noreply.github.com>
2024-03-16 18:37:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def get_anime_episodes():
         return jsonify({'error': 'Nothing was found'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
