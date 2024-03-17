7b006928f7ad3db91485595a9a189e10654cae89
Untitled Master <128633214+Untitled-Master@users.noreply.github.com>
2024-03-17 18:43:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
